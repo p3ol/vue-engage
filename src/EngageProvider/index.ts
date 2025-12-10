f7ebec8d8c9d0334a2cb2610b828f3620245b74e
Nicolas AUGER <nicolas@poool.fr>
2025-12-10 11:52:36 +0100

    fix: wrong engage provider behavior
    BUG-1106
    
    Co-Authored-By: Simon Deflesschouwer <43884154+defless@users.noreply.github.com>

--- src/EngageProvider/index.ts
+++ src/EngageProvider/index.ts
@@ -87,7 +87,6 @@ const EngageProvider = defineComponent({
       default: false,
     },
   },
-
   data: () => {
     return {
       lib: null as Poool.Engage | null,
@@ -97,10 +96,10 @@ const EngageProvider = defineComponent({
 
   async mounted() {
     if (
-      !globalThis.Engage ||
-      !globalThis.Engage.isPoool ||
-      !globalThis.PooolEngage ||
-      !globalThis.PooolEngage.isPoool
+      !globalThis.Engage &&
+      !globalThis.Engage?.isPoool &&
+      !globalThis.PooolEngage &&
+      !globalThis.PooolEngage?.isPoool
     ) {
       await loadScript(this.scriptUrl, 'poool-vue-engage-lib', {
         timeout: this.scriptLoadTimeout,
@@ -179,9 +178,9 @@ const EngageProvider = defineComponent({
           }
         });
 
-        trace('EngageProvider', this.vueDebug,
-          'Engage factory initialized & setup successfuly'
-        );
+      trace('EngageProvider', this.vueDebug,
+        'Engage factory initialized & setup successfuly'
+      );
 
       return factory;
     },
